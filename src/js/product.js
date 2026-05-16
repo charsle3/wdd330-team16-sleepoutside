6d8606c3d6d761bdc538cc745be4064882f769e8
charsle3 <jpkingthe3rd@gmail.com>
2026-05-15 20:39:05 -0700

    week 2 team activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,31 +1,9 @@
-import { setLocalStorage } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getParam } from "./utils.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-//get existing cart items and (if they exist) add them to an array with the item to be added, and save it all to localstorage
-function addProductToCart(product) {
-  const existingCart = getLocalStorage("so-cart");
-  const products = [];
-
-  if (existingCart != null) {
-    existingCart.forEach((item) => {
-      products.push(item);
-    });
-  }
-
-  products.push(product);
-
-  setLocalStorage("so-cart", products);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const specificProduct = new ProductDetails(productID, dataSource);
+specificProduct.init();
