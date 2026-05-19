7677484ca5a997da643ba241a674f0e11e8ba2a1
charsle3 <jpkingthe3rd@gmail.com>
2026-05-19 14:56:40 -0700

    fixing images

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-
+ 
 export default defineConfig({
+  base: "/",
   root: "src/",
-
+  publicDir: "../public",
   build: {
     outDir: "../dist",
     rollupOptions: {
